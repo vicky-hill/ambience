0cd63bf3d87d234f3fac0d73f773f3aea8577062
vicky-hill <vicky.ungemach95@gmail.com>
2025-09-24 06:07:01 -0700

    add video utils

--- app/latte/page.tsx
+++ app/latte/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import YouTube from 'react-youtube'
+import videos from '@/utils/videos'
 
 interface page {
 
@@ -11,31 +12,14 @@ const opts = {
     width: 300
 }
 
-const latteVideos: string[] = [
-    '4CkTBn014hw',
-    'qO-K3D9BdzY',
-    'dRHCAgN6LZw',
-    'DoirTSeqX5A',
-    'vPOLJ-MDIyI',
-    'mQv1qjIhhOE',
-    'bPTSdK-GDPM',
-    'QDrXHP8gLio',
-    // '',
-    // '',
-    // '',
-    // '',
-    // '',
-    // '',
-    // '',
-]
 
 export default function page({ }: page) {
 
     return (
         <div className='p-10 flex gap-10 flex-wrap justify-between'>
             {
-                latteVideos.map(video => (
-                    <YouTube key={video} videoId={video} opts={opts} />
+                videos.latte.map((video: any) => (
+                    <YouTube key={video.videoId} videoId={video.videoId} opts={opts} />
                 ))
             }
         </div>
---
+++ utils/videos.ts
@@ -0,0 +1,58 @@
+const videos = {
+    latte: [
+        { title: 'Vietnamese Full Care Course 🌺✨', videoId: '4CkTBn014hw'},
+        { title: 'ASMR Soft Haircut & Makeup with Wooden Tools 🎀', videoId: 'qO-K3D9BdzY'},
+        { title: 'My Favorite Personal Attention Triggers To You 💜', videoId: 'iY0Pu0u2Yzs'},
+        { title: 'Let Me Clean Your Face Well 😊', videoId: 'VDJjOC0hNGI'},
+        { title: 'Healing Pamper Night 🌙', videoId: 'II_jHdL9AW0'},
+        { title: 'Sleepy Eyelash Extensions & Lip Treatment 💜', videoId: 'rLj3hkPSaAk'},
+        { title: 'Take a Cool Nap in a Summer Day 🍉', videoId: 'KIvtRAXr-QY'},
+        { title: 'Relaxing Face Treatment 😌', videoId: 'xqoRFiLOweo'},
+        { title: 'Only For Your Sleep 🌙', videoId: 'DoirTSeqX5A'},
+        { title: 'ASMR Backstage Idol Makeup & Hair Styling 🎤🎀', videoId: 'N6ZApDpK2Gc'},
+        { title: 'Doing Your Lovely Pink Makeup 🎀', videoId: 'JcSrOpFKfQM'},
+        { title: 'ASMR Face Lift & Contour Massage', videoId: 'vPOLJ-MDIyI'},
+        { title: 'ASMR Dermaplaning Facial Treatment', videoId: 'mQv1qjIhhOE'},
+        { title: 'ASMR Physical Therapy & Aroma Massage 💤', videoId: 'fk-YH0cWc6U'},
+        { title: 'ASMR Chinese Eye Cleaning 👁️✨', videoId: 'bPTSdK-GDPM'},
+        { title: 'Sleep With Good Mood 💓', videoId: 'ZD67afkURFQ'},
+        { title: 'ASMR Autumn Night 🍂', videoId: 'AcLyZUs9hgg'},
+        { title: 'Summer Ocean Spa 🌊', videoId: 'EiQ7X7rzeAk'},
+        { title: 'Spring Flower Head Spa 🌸', videoId: '_CPSbDlAVlY'},
+        { title: 'Healing Facial Therapy 🌿', videoId: '_AxKXEm1vOI'},
+        { title: 'ASMR Relaxing Face Adjustment 🌟', videoId: 'JYbSw1_kgLg'},
+        { title: 'ASMR Night Nurse Taking Care of You 🩺', videoId: 'wwDL6Kxu1xA'},
+        { title: 'ASMR Facial Lymph Drainage Massage 🌷', videoId: '37ASrTbntxA'},
+        { title: 'ASMR Delicate & In-Depth Facial Skin Exam', videoId: 'Q089kFWhP5Q'},
+        { title: 'SMR Sleep Therapist Visit 🌙', videoId: 'BDHgeoRpPy0'},
+        { title: 'ASMR Rlaxing Sleep Exam 💤', videoId: 'HeCSMKuvmvM'},
+        { title: 'ASMR Relax with gentle massage care', videoId: 'sFnLvZmyx48'},
+        { title: 'ASMR Energy Healing clinic 🔮', videoId: '-Tu1jjB5KWM'},
+        { title: 'ASMR Comprehensive Medical Exam', videoId: 'FMFDl_S2mOQ'},
+        { title: 'Sleepy Facial Treatment ✨', videoId: 'Jn8r8kUB9Mo'},
+        { title: 'Plant Doctors Office 🌿', videoId: 'mVfvtf2-E7Q'},
+        { title: 'ASMR Palpating Your Face and Scalp 👐', videoId: 'UiDqTDgtxsI'},
+        { title: 'ASMR Dream Makeup Salon 💗', videoId: 'rLEBL4RibcA'},
+        { title: 'ASMR Flight Attendant Sleep Care Service ✈️', videoId: 'JaqesAupcVA'},
+        { title: 'ASMR Detailed Face Analysis For Your Sleep', videoId: 'D32F06ggHmQ'},
+        { title: 'ASMR School Nurses Office 💛', videoId: 'aI0Ub6ApXv4'},
+        { title: 'Relaxing ASMR Clinic 😌', videoId: 'fGPzNcyvYlw'},
+        { title: 'ASMR Full Body Physical Exam', videoId: '7chmbcMe0H4'},
+        { title: 'ASMR Ear, Nose & Throat Examination', videoId: 'PHPbj9laf28'},
+        { title: 'ASMR Face Mapping Clinic 😌', videoId: 'ru0apZSeNxc'},
+        { title: 'Botox Treatment & Lip Fillers 💉', videoId: 'orRJPguYG1s'},
+        { title: 'Measuring Your Face 📏', videoId: 'JgK51SSymKk'},
+        { title: 'Im Your Sleep Doctor Tonight 💕', videoId: 'QlexpFO6SzU'},
+        { title: 'ASMR Cranial Nerve Exam 📋', videoId: 'iu9kNDArCuw'},
+        { title: 'ASMR Eye Relaxation Clinic 🌼', videoId: 'zUMPpjlauwU'},
+        { title: 'School Nurse Lice Check 💙', videoId: 'b2MS4lyITk8'}
+    ],
+    ambience: [
+
+    ],
+    reading: [
+
+    ]
+}
+
+export default videos;
